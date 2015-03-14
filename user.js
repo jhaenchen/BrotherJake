a95d7197a88004e438d5fa2ae4853f448c836e1b
Jackson Haenchen <jackson_haenchen@gmail.com>
2015-03-14 14:32:16 +0100

    initial commit

---
+++ user.js
@@ -0,0 +1,7 @@
+$('#carousel-demo1').scrollingCarousel();
+		
+		$('#carousel-demo2').scrollingCarousel( {
+			scrollerAlignment : 'vertical',
+			autoScroll: true,
+            autoScrollSpeed: 15000
+		});
